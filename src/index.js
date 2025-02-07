3aa68fba7cf7b36d66c4075da8c9d3826333de71
Irwin Pratajaya <irwinpratajaya@gmail.com>
2025-02-07 18:26:45 +0700

    feat: add fetchUserInfo

--- src/index.js
+++ src/index.js
@@ -1,28 +1,33 @@
 require('dotenv').config();
-const { createHeaders, getUserId, fetchUserPosts } = require('./api/instagram');
+const { createHeaders, getUserInfo, fetchUserPosts } = require('./api/instagram');
 const { handleError } = require('./utils/errorHandler');
 const { validateUsername, validateCookie } = require('./utils/validator');
 const logger = require('./utils/logger');
 
 /**
- * Fetches image URLs from a public Instagram profile
- * @param {string} username - The Instagram username to fetch images from
- * @returns {Promise<string[]>} Array of image URLs
+ * Fetches profile information and image URLs from a public Instagram profile
+ * @param {string} username - The Instagram username to fetch data from
+ * @returns {Promise<Object>} Object containing profile info and image URLs
  */
-async function getInstagramImages(username) {
+async function getInstagramData(username) {
   try {
     validateUsername(username);
     validateCookie(process.env.INSTAGRAM_COOKIE);
 
     const headers = createHeaders(username, process.env.INSTAGRAM_COOKIE);
-    const userId = await getUserId(username, headers);
-    return await fetchUserPosts(userId, headers);
+    const profileInfo = await getUserInfo(username, headers);
+    const images = await fetchUserPosts(profileInfo.id, headers);
+
+    return {
+      profile: profileInfo,
+      images: images
+    };
 
   } catch (error) {
     handleError(error);
-    return [];
+    return { profile: null, images: [] };
   }
 }
 
 // Export the main function
-module.exports = { getInstagramImages };
+module.exports = { getInstagramData };
